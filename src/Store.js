b43fddc9ac6f553bc830b4382e5f0536a10a5d5e
juaneth <euanwalker2008@gmail.com>
2023-04-16 01:03:09 +0100

    Changed discord invite colour, more work on storage

--- src/Store.js
+++ src/Store.js
@@ -1,4 +1,12 @@
-export const AddNewBot = (name, path, token) => {}
+const storage = window.localStorage
+
+export const AddNewBot = (name, path, token) => {
+    if (storage.getItem(`bots.${name}`)) {
+        return "Name Exists"
+    }
+
+    storage.setItem(name, path)
+}
 export const RemoveBot = (name, path) => {}
 
 export const ManageBot = (name, path) => {}
